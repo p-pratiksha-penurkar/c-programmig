6ba8820c9df0cab34eab262bd20d6d80f3149b1e
pratiksha prasad penurkar <pratikshaprasadp952001@gmail.com>
2026-02-20 14:24:26 +0530

    Create Integer.c
    Add a simple C program to demonstrate integer usage.

---
+++ Data_Types_in_c/Primary_data_type/Integer.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //input output file
+
+int main()   //entry point function
+{
+  
+    int var = 22;  //integer value
+    printf("var = %d", var);   //printf msg on terminal
+    
+  return 0;  //return response-sucessful
+                // accepted output var=22
+
